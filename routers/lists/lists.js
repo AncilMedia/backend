c1b69a7b140eae3adf260a09c84e8b2c6566891a
Ancil Media <akhil7ancilmedia@gmail.com>
2025-06-30 17:15:56 +0530

    lists

--- routers/lists/lists.js
+++ routers/lists/lists.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const List = require('../../lists/list');
+const List = require('../../models/lists/list');
 
 // ✅ GET all lists
 router.get('/', async (req, res) => {
